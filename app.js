6384d9dac9fa425cc783715d64993074ad62a2c4
Nabeel Shahzad <nabeel@nsslive.net>
2012-02-22 21:56:47 -0500

    node modules, updateS

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+//qwklst app
+
+var Config = {
+	host_port: 4000
+};
+
 var express = require('express');
 
 var app = express.createServer();
-
-app.get('/', function(req, res) {
-    resp.send('hello');
+app.configure(function() {
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.bodyParser());
+	app.set('views', __dirname + '/views');
 });
 
-app.listen(4000);
+app.get('/', function(req, resp) {
+	resp.send('hello');
+});
+
+app.get('/:prefix/:listid', function(req, resp) {
+	resp.send("prefix: " + req.params.prefix + ", listid: " + req.params.listid);
+});
+
+app.listen(Config.host_port);
